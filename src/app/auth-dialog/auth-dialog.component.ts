8493390d2a040dea7dc9f6491da557fc0408d173
Javier Jimenez <javierrcc522@gmail.com>
2017-12-14 11:09:36 -0800

    add auth, home and toolbar componenents

---
+++ src/app/auth-dialog/auth-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-dialog',
+  templateUrl: './auth-dialog.component.html',
+  styleUrls: ['./auth-dialog.component.sass']
+})
+export class AuthDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
